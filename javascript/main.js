6c04d6d58ba83d495f149ac7e4e2521b7539ec6f
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-05-17 17:52:35 +0200

    [FEAT]aggiunta stampa risultato su pagina

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,8 @@
 let tratta = parseInt(prompt ('quanti km devi percorrere?'));
 console.log(tratta)
-
 let eta = parseInt(prompt ('Quanti anni hai?'));
 console.log(eta)
-
 let costo_km = 0.21;
-
 let prezzo = (tratta * costo_km).toFixed(2);
 let prezzo_finale;
 
@@ -18,15 +15,18 @@ if ( isNaN(tratta)|| isNaN(eta) || (tratta === 0) || (eta === 0)) {
     let sconto_1 = prezzo * 0.20;
     prezzo_finale = (prezzo - sconto_1).toFixed(2);
     console.log( `il tuo biglietto costa ${prezzo_finale} €`)
+    document.getElementById("content").innerHTML= `Il tuo biglietto costa: ${prezzo_finale} €`;
 } else {
 // sconto over 65
     if ( eta > 65) {
         let  sconto_2 = prezzo * 0.40
         prezzo_finale = (prezzo - sconto_2).toFixed(2)
-        
+        document.getElementById("content").innerHTML= `Il tuo biglietto costa: ${prezzo_finale} €`;
         console.log( `il tuo biglietto costa ${prezzo_finale} €`)
+        
             // prezzo pieno
-        } else{
-            console.log( `il tuo biglietto costa ${prezzo} €`)
+    } else{
+        console.log( `il tuo biglietto costa ${prezzo} €`)
+        document.getElementById("content").innerHTML= `Il tuo biglietto costa: ${prezzo} €`;
     }
 }
