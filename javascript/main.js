b5380defea79c9d5c3f05d200d76bf4bbab28363
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-05-17 17:44:44 +0200

    [FEAT]aggiunto controllo ad if per numeri === 0

--- javascript/main.js
+++ javascript/main.js
@@ -6,16 +6,27 @@ console.log(eta)
 
 let costo_km = 0.21;
 
-let prezzo = tratta * costo_km;
+let prezzo = (tratta * costo_km).toFixed(2);
+let prezzo_finale;
 
 
 
-console.log( `il tuo biglietto costa ${prezzo} €` )
-
-if ( isNaN(tratta)|| isNaN(eta) ){
-    console.log("uno dei due valori non è un numero")
+if ( isNaN(tratta)|| isNaN(eta) || (tratta === 0) || (eta === 0)) {
+    console.log("i due valori possono essere solo un numero diverso da zero")
+    // sconto minorenni
 } else if (eta < 18 ) {
-    let sconto_1 = prezzo * 0.20
-//    console.log( `il tuo biglietto costa ${prezzo * 0.20}`)
-console.log(sconto_1)
+    let sconto_1 = prezzo * 0.20;
+    prezzo_finale = (prezzo - sconto_1).toFixed(2);
+    console.log( `il tuo biglietto costa ${prezzo_finale} €`)
+} else {
+// sconto over 65
+    if ( eta > 65) {
+        let  sconto_2 = prezzo * 0.40
+        prezzo_finale = (prezzo - sconto_2).toFixed(2)
+        
+        console.log( `il tuo biglietto costa ${prezzo_finale} €`)
+            // prezzo pieno
+        } else{
+            console.log( `il tuo biglietto costa ${prezzo} €`)
+    }
 }
